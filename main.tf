a345ef712f3ce5ae36a3c6aafb1a74941816b6a3
Manoj Jonnakuti <jonnakutimanoj90@gmail.com>
2024-05-21 14:47:56 +0530

    terraform code update for my app

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.50.0"
+    }
+  }
+}
 resource "aws_instance" "ec2" {
   ami = var.id
   instance_type = var.type
